33f24b3e73de2abdde764272370d62b525b0e12a
WayneInwonHan <blissgram@gmail.com>
2021-05-08 23:54:39 -0500

    Add 2 algorithm question answers

---
+++ Linked List/deleteNodeInALinkedList.py
@@ -0,0 +1,26 @@
+class ListNode:
+   def __init__(self, data, next = None):
+      self.val = data
+      self.next = next
+def make_list(elements):
+   head = ListNode(elements[0])
+   for element in elements[1:]:
+      ptr = head
+      while ptr.next:
+         ptr = ptr.next
+      ptr.next = ListNode(element)
+   return head
+def print_list(head):
+   ptr = head
+   print('[', end = "")
+   while ptr:
+      print(ptr.val, end = ", ")
+      ptr = ptr.next
+   print(']')
+class Solution(object):
+   def deleteNode(self, node, data):
+
+      while node.val is not data:
+         node = node.next
+      node.val = node.next.val
+      node.next = node.next.next
--- String/stringToInteger.py
+++ String/stringToInteger.py
@@ -1,6 +1,6 @@
 def myAtoi(self, s: str) -> int:
-        int_min=-(2**31)
-        int_max=(2**31)-1
+        INT_MIN=-(2**31)
+        INT_MAX=(2**31)-1
         
         s=s.strip()
         
@@ -19,9 +19,9 @@ def myAtoi(self, s: str) -> int:
         
         if not first or first in {'-','+'}:
             first=0
-        elif int(first)<int_min:
-            first=int_min
-        elif int(first)>int_max:
-            first=int_max
+        elif int(first)<INT_MIN:
+            first=INT_MIN
+        elif int(first)>INT_MAX:
+            first=INT_MAX
             
         return int(first)
