3e39ce70a9dae1055a838260894cdc4f5cfa0ac6
WayneInwonHan <blissgram@gmail.com>
2021-05-13 22:03:39 -0500

    Add 2 algorithm question answers

---
+++ SortingAndSearching/firstBadVersion.py
@@ -0,0 +1,19 @@
+first_bad = 0
+def isBadVersion(version):
+    if version >= first_bad:
+        return True
+    return False
+class Solution:
+    def firstBadVersion(self, n):
+        if n <2:
+            return n
+        start = 1
+        end = n
+        while(start<=end):
+            mid = (start+end)//2
+            if isBadVersion(mid) and not isBadVersion(mid-1):
+                return mid
+            elif isBadVersion(mid-1):
+                end = mid-1
+            else:
+                start = mid+1
