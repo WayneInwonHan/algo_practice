33b3011bdc8a239e3a59c284d2dbc452861e0e4d
WayneInwonHan <blissgram@gmail.com>
2021-05-22 23:52:31 -0500

    Add 1 algorithm question answer

---
+++ maxConsecutiveOnes.py
@@ -0,0 +1,12 @@
+def findMaxConsecutiveOnes(self, nums):
+    if not nums:
+        return 0
+    max_o = 0
+    c = 0
+    for i in nums:
+        if i == 1:
+            c+=1
+        else:
+            max_o = max(c,max_o)
+            c = 0
+    return max(c,max_o)
