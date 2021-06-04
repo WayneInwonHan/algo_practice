c694b9c31b6123f0e5d99dc4291f4d35e1692c52
WayneInwonHan <blissgram@gmail.com>
2021-06-04 02:06:15 -0500

    Add 1 algorithm question answer

---
+++ replaceElementsWithGreatestElementOnRightSide.py
@@ -0,0 +1,6 @@
+def replaceElements(self, arr: List[int]) -> List[int]:
+    answer = [0] * len(arr)
+    answer[-1] = -1
+    for i in range(len(arr)-2, -1, -1):
+        answer[i] = max([answer[i+1], arr[i+1]]) 
+    return answer
