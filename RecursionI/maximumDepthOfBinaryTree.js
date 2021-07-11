2fc969d08173fec40ca95272b641cd439eff34ef
WayneInwonHan <blissgram@gmail.com>
2021-07-10 21:06:19 -0500

    Add 1 algorithm question answer

--- RecursionI/maximumDepthOfBinaryTree.js
+++ RecursionI/maximumDepthOfBinaryTree.js
@@ -3,9 +3,7 @@ var maxDepth = function(root) {
         var max=0;
         if(node!=null){
             max++;
-            //The depth of the left subtree
             var ml=bl(node.left);
-            //The depth of the right subtree
             var mr=bl(node.right);
             max+=ml>mr?ml:mr;        
         }
